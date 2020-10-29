3a2f1b43209320bc1d0529d434ddeedce553a1e0
Ansh028 <71138619+Ansh028@users.noreply.github.com>
2020-10-29 22:50:08 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# staticSquare
+A static square made using Matter.js physics engine
+heloo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'pls                          emjqwlkfn;lnkgeneihqnraSKLw;ibuA .,LMHNUJ
---
+++ RajmaChawal.js
@@ -0,0 +1,3 @@
+//DUE TO SOME PROBLEMS THIS IS THE RECIPE OF DOSA\
+
+//Blend 2 cups of leftover rice, 1 cup of suji/semolina and 1/2 cup of curd. Whisk the mixture well and pour water to a smooth dosa batter. Leave aside for 15 minutes. Make dosas by stuffing grated paneer, beetroot, green chillies, coriander leaves, onion and carrot. Drizzle some ghee over it and serve crispy dosas. 😍
